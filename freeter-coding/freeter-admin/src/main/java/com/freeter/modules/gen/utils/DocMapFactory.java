7a13eca17b3312a08e6ce6f1fc9d2522f549e888
171998110@qq.com <Administrator@Wl-08>
2018-07-23 17:41:33 +0800

    代码生成器移植

---
+++ freeter-coding/freeter-admin/src/main/java/com/freeter/modules/gen/utils/DocMapFactory.java
@@ -0,0 +1,46 @@
+package com.freeter.modules.gen.utils;
+
+import java.sql.SQLException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.sql.DataSource;
+
+import com.freeter.common.utils.SpringContextUtils;
+import com.freeter.modules.gen.entity.TableEntity;
+import com.freeter.modules.gen.service.MysqlDataBaseService;
+import com.freeter.modules.gen.service.OracleDataBaseService;
+
+ 
+public class DocMapFactory {
+
+	public static Map build() throws SQLException{
+		Map map = new HashMap();
+		DataSource dataSource =(DataSource) SpringContextUtils.getBean("dataSource");
+		String databaseProductName = dataSource.getConnection().getMetaData().getDatabaseProductName();
+		 if(("MySQL").equals(databaseProductName)){
+				
+			MysqlDataBaseService mysqlDataBaseService = (MysqlDataBaseService) SpringContextUtils.getBean("mysqlDataBaseService");
+  				String databaseName = mysqlDataBaseService.queryDatabaseName();
+				List<TableEntity> tableInfos = mysqlDataBaseService.getTableList();
+ 			
+				map.put("tableInfos", tableInfos);
+				map.put("databaseName", databaseName);
+				 
+		 		 
+		 } else  if(("Oracle").equals(databaseProductName)){
+				
+			 OracleDataBaseService oracleDataBaseService = (OracleDataBaseService) SpringContextUtils.getBean("oracleDataBaseService");
+				String databaseName = oracleDataBaseService.queryDatabaseName();
+				List<TableEntity> tableInfos = oracleDataBaseService.getTableList();
+			
+				map.put("tableInfos", tableInfos);
+				map.put("databaseName", databaseName);
+				 
+		 		 
+		 } 
+		 
+		 return map;
+	}
+}
