ea07b4d7d5d6f71541fb8348615dbe53c41f4f0a
171998110@qq.com <Administrator@Wl-08>
2018-07-27 13:39:39 +0800

    用户也可能为空

--- freeter-coding/freeter-admin/src/main/java/com/freeter/MyMetaObjectHandler.java
+++ freeter-coding/freeter-admin/src/main/java/com/freeter/MyMetaObjectHandler.java
@@ -21,14 +21,19 @@ public class MyMetaObjectHandler extends MetaObjectHandler {
 	    public void insertFill(MetaObject metaObject) {
    	        logger.info("新增的时候干点不可描述的事情");
 	        setFieldValByName("createTime", new Date(), metaObject);
+	        if( ShiroUtils.getUserEntity()!= null) {
 	        setFieldValByName("createBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
-	    }
+	        }
+	        }
 
 	    @Override
 	    public void updateFill(MetaObject metaObject) {
 	        logger.info("更新的时候干点不可描述的事情");
+	        
 	        setFieldValByName("updateTime", new Date(), metaObject);
+	        if( ShiroUtils.getUserEntity()!= null) {
 	        setFieldValByName("updateBy", ShiroUtils.getUserEntity().getUsername(), metaObject);
+	        }
 	    }
 
 }
