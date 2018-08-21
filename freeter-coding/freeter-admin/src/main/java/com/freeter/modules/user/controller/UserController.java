ceed2b8bb3a1701c68fafddc29c91e1c50b02a4a
171998110@qq.com <Administrator@Wl-08>
2018-08-21 14:10:07 +0800

    会员模块代码提交

--- freeter-coding/freeter-admin/src/main/java/com/freeter/modules/user/controller/UserController.java
+++ freeter-coding/freeter-admin/src/main/java/com/freeter/modules/user/controller/UserController.java
@@ -1,25 +1,28 @@
 package com.freeter.modules.user.controller;
 
 import java.util.Arrays;
-import java.util.Map;
 
+import com.freeter.common.validator.ValidatorUtils;
 import org.apache.shiro.authz.annotation.RequiresPermissions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
-import com.freeter.common.utils.MPUtil;
+import com.freeter.common.utils.JQPageInfo;
+import com.freeter.common.mpextend.parser.ParseWrapper;
+import com.freeter.common.utils.PageInfo;
+
+
+import com.freeter.modules.user.entity.UserEntity;
+import com.freeter.modules.user.entity.view.UserView;
+import com.freeter.modules.user.entity.model.UserModel;
+
+import com.freeter.modules.user.service.UserService;
 import com.freeter.common.utils.PageUtils;
 import com.freeter.common.utils.R;
-import com.freeter.common.validator.ValidatorUtils;
-import com.freeter.modules.user.entity.UserEntity;
-import com.freeter.modules.user.entity.model.UserModel;
-import com.freeter.modules.user.entity.view.UserView;
-import com.freeter.modules.user.service.UserService;
+import com.freeter.common.utils.MPUtil;
 
 
 
@@ -28,10 +31,11 @@ import com.freeter.modules.user.service.UserService;
  * 后端接口
  * @author xuchen
  * @email 171998110@qq.com
- * @date 2018-06-30 13:40:24
+ * @date 2018-08-21 14:03:28
  */
 @RestController
 @RequestMapping("user/user")
+@SuppressWarnings({"unchecked","rawtypes"})
 public class UserController {
     @Autowired
     private UserService userService;
@@ -41,10 +45,10 @@ public class UserController {
      */
     @RequestMapping("/page")
     @RequiresPermissions("user:user:list")
-    public R page(@RequestParam Map<String, Object> params,UserModel userModel){
- 
-       
-    	PageUtils page = userService.queryPage(params, userModel);
+    public R page(JQPageInfo jqPageInfo,UserModel user){
+        EntityWrapper< UserEntity> ew = ParseWrapper.parseWrapper(user);
+     	PageInfo pageInfo = new PageInfo(jqPageInfo);
+     	PageUtils page = userService.queryPage(pageInfo, ew);
     
         return R.ok().put("page", page);
         
@@ -55,9 +59,8 @@ public class UserController {
      */
     @RequestMapping("/list")
     @RequiresPermissions("user:user:list")
-    public R list( UserEntity user){
-       	EntityWrapper<  UserEntity> ew = new EntityWrapper<  UserEntity>();
-      	ew.allEq(MPUtil.allEQMapPre( user, "user")); 
+    public R list( UserModel user){
+       	EntityWrapper< UserEntity> ew = ParseWrapper.parseWrapper(user);
         return R.ok().put("data",  userService.selectListView(ew));
     }
 
